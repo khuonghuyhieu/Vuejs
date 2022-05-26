170c87b43aa26ba85201ec41fa090ba1e505b006
Khương Huy Hieu <khuonghuyhieu0705@gmail.com>
2022-05-27 00:27:39 +0700

    page-scenes-demo

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="row container-scenes mt-5">
+    <HelloWorld
+      v-for="(item, index) in items"
+      :key="index"
+      :scenes-data="item"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      items: [
+        {
+          'image': "1.jpg",
+          nameScenes: "Breakfast",
+          nameGroup: [
+            "Linear",
+            "Ledstrip",
+            "Track White",
+            "Single Channel",
+            "Track Black",
+          ],
+        },
+        {
+          image: "2.jpg",
+          nameScenes: "Conference",
+          nameGroup: [
+            "Linear",
+            "Ledstrip",
+            "Track White",
+            "Single Channel",
+            "Track Black",
+          ],
+        },
+        {
+          image: "3.jpg",
+          nameScenes: "Dinner",
+          nameGroup: [
+            "Linear",
+            "Ledstrip",
+            "Track White",
+            "Single Channel",
+            "Track Black",
+          ],
+        },
+        {
+          image: "4.jpg",
+          nameScenes: "Lunch",
+          nameGroup: [
+            "Linear",
+            "Ledstrip",
+            "Track White",
+            "Single Channel",
+            "Track Black",
+          ],
+        },
+        {
+          image: "5.jpg",
+          nameScenes: "Meeting",
+          nameGroup: [
+            "Linear",
+            "Ledstrip",
+            "Track White",
+            "Single Channel",
+            "Track Black",
+          ],
+        },
+        {
+          image: "6.jpg",
+          nameScenes: "Party",
+          nameGroup: [
+            "Linear",
+            "Ledstrip",
+            "Track White",
+            "Single Channel",
+            "Track Black",
+          ],
+        },
+        {
+          image: "7.jpg",
+          nameScenes: "Presentation",
+          nameGroup: [
+            "Linear",
+            "Ledstrip",
+            "Track White",
+            "Single Channel",
+            "Track Black",
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="itemList border d-flex justify-content-between">
+    <div class="itemList__info d-flex">
+      <img class="border" :src="scenesData.image" alt="place a wait" />
+      <div class="itemList__name">
+        <h5 class="mb-3">{{ scenesData.nameScenes }}</h5>
+        <button
+          type="button"
+          class="btn btn-primary"
+          v-for="(group, index) in {{scenesData.nameGroup}}"
+          :key="index"
+        >
+          name of group
+        </button>
+      </div>
+    </div>
+    <div class="itemList__buttonEdit align-self-end">
+      <button type="button" class="btn btn-primary">Edit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    scenesData: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+
+  // props: ["scenesData"] // Nếu k có định dạng thì để nv cũng đc
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
